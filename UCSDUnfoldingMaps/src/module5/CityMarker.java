f4cb462bbd202afa4c918d49cb4bc5f456381451
JonJoo57 <47547628+JonJoo57@users.noreply.github.com>
2020-01-19 23:54:46 -0600

    week5 module 1/19/2020

--- UCSDUnfoldingMaps/src/module5/CityMarker.java
+++ UCSDUnfoldingMaps/src/module5/CityMarker.java
@@ -16,7 +16,7 @@ import processing.core.PGraphics;
 // TODO: Change SimplePointMarker to CommonMarker as the very first thing you do 
 // in module 5 (i.e. CityMarker extends CommonMarker).  It will cause an error.
 // That's what's expected.
-public class CityMarker extends SimplePointMarker {
+public class CityMarker extends CommonMarker {
 	
 	public static int TRI_SIZE = 5;  // The size of the triangle marker
 	
@@ -35,23 +35,24 @@ public class CityMarker extends SimplePointMarker {
 	/**
 	 * Implementation of method to draw marker on the map.
 	 */
-	public void draw(PGraphics pg, float x, float y) {
-		// Save previous drawing style
-		pg.pushStyle();
-		
-		// IMPLEMENT: drawing triangle for each city
-		pg.fill(150, 30, 30);
-		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
-		
-		// Restore previous drawing style
-		pg.popStyle();
-	}
+//	public void draw(PGraphics pg, float x, float y) {
+//		// Save previous drawing style
+//		pg.pushStyle();
+//		
+//		// IMPLEMENT: drawing triangle for each city
+//		pg.fill(150, 30, 30);
+//		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
+//		
+//		// Restore previous drawing style
+//		pg.popStyle();
+//	}
 	
 	/** Show the title of the city if this marker is selected */
 	public void showTitle(PGraphics pg, float x, float y)
 	{
-		
-		// TODO: Implement this method
+		pg.text(getCity(), x, y);
+		pg.text(getCountry(), x, y+10);
+		pg.text(getPopulation(), x, y+20);
 	}
 	
 	
@@ -72,4 +73,30 @@ public class CityMarker extends SimplePointMarker {
 	{
 		return Float.parseFloat(getStringProperty("population"));
 	}
+
+
+	/*
+	 * So why does the draw method in this class needs to be removed before running the app
+	 * so that the markers can be covered over?
+	 * 
+	 * Answer:
+	 * 1. it is duplicating the code to show the marker.
+	 * 2. CommonMarker is the super class the overrides(polymorphism) the draw method.
+	 * Here the draw method was already in place. Thus needed to be removed. 
+	 */
+	
+	@Override
+	public void drawMarker(PGraphics pg, float x, float y) {
+		
+		pg.pushStyle();
+
+		// IMPLEMENT: drawing triangle for each city
+		pg.fill(150, 30, 30);
+		pg.triangle(x, y - TRI_SIZE, x - TRI_SIZE, y + TRI_SIZE, x + TRI_SIZE, y + TRI_SIZE);
+
+		// Restore previous drawing style
+		pg.popStyle();
+	}
+	
+	
 }
